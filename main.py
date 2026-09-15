3925d43331e0c8a7dadd4977b00f9d702ae31547
hehehahahoobausa <bausa.gb@obmontessori.edu.ph>
2026-09-16 06:49:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pyscript import document, display
 
 def create_order(e):
-    document.getElementById("Subtotal").innerHTML = "" # clears previous output
+    document.getElementById("Subtotal").innerHTML = "" # clears previous output, applies to VAT TAX and Total Amount
     document.getElementById("VAT TAX").innerHTML = ""
     document.getElementById("Total Amount").innerHTML = ""
 
-    coffee = document.getElementById('Coffee') #
+    coffee = document.getElementById('Coffee') 
     coffeeprice = float(coffee.value) * coffee.checked
 
     icedmilo = document.getElementById('Iced Milo')
@@ -20,12 +20,12 @@ def create_order(e):
     macchiato = document.getElementById('Macchiato')
     macchiatoprice = float(macchiato.value) * macchiato.checked
 
-    subtotal = coffeeprice + icedmiloprice + milkprice + mochaprice + macchiatoprice #it is a combination of all, because the code will be read on the basis of the checked boxes, so if coffee is checked but the rest arent, only coffee's price would be inputted
-    vat = subtotal * 0.12 #12% = 0.12, multiply to get the tax added
-    total = vat + subtotal # adds vat (product) and subtotal (sum)
+    sub = coffeeprice + icedmiloprice + milkprice + mochaprice + macchiatoprice #it is a combination of all, because the code will be read on the basis of the checked boxes, so if coffee is checked but the rest arent, only coffee's price would be inputted
+    vat = sub * 0.12 #12% = 0.12, multiply to get the tax added
+    total = vat + sub # adds vat (product) and sub (sum)
 
 
-    Sub = f"Subtotal: ₱{subtotal:.2f}"
+    Sub = f"Subtotal: ₱{sub:.2f}"
     display(Sub, target = "Subtotal") #targets div "Subtotal" to display result. the same case for VAT and Total
 
     Vat = f"VAT: ₱{vat:.2f}"
